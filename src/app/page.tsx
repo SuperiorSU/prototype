ccd9ee61a182a83499ce2d6674358cb3dd7921d8
SuperiorSU <sujalsinha2001@gmail.com>
2024-03-14 00:28:45 +0530

    Mess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Card from "../components/Card"
 import hulk from "../assets/hulkbg.jpg"
 import iron from "../assets/ironmanbg.jpg"
 import spider from "../assets/spidermanbg.jpg"
+import Vision from "../assets/visionbg.jpg"
+
 // const raleway = Raleway({
 //   subsets:['latin'],
 //   weight:['900'],
@@ -14,7 +16,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between border- bg-black">
       <div className="w-full h-[60vh] bg-red-500">
         <div className="flex flex-row flex-wrap gap-3">
-          <div className="h-[100%] w-[1px] bg-white"></div>
+          <div className="w-[1px] bg-white/50 ms-[30px]"></div>
           <div className=" p-4 ps-20 w-[45%]">
             <div className="mt-[30%]">
               <h1 className="text-white text-[32px] text-start font-bold">Welcome</h1>
@@ -36,6 +38,18 @@ export default function Home() {
         <Card img={spider}/>
       </div>
 
+      </div>
+      <div className="p-5 mt-5 text-white">
+        <div className="grid grid-cols-5 gap-3">
+          <div className="col-span-2 border-[.5px] border-white/50 p-5 ">
+            <h3 className="font-bold text-white text-[64px] border-t-[.4px] border-white/65">We're <span className="text-red-500 [text-shadow:_0_6px_0_rgb(255_255_255_/_40%)]">AVENGERS</span> </h3>
+            <br/>
+            <p className="text-justify pe-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem similique ea veritatis animi temporibus nostrum nam, sunt illum consequatur, quae ut, qui sequi? Architecto ut sapiente praesentium dolorem aspernatur consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga optio animi eligendi ducimus eius, ab delectus velit et voluptas officiis?</p>
+          </div>
+          <div className="col-span-3 border-[.5px] border-white">
+            <Image src={Vision} alt="" className=""/>
+          </div>
+        </div>
       </div>
       
       
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,12 @@ import Image from 'next/image'
 const Card = ({img}) => {
   return (
     <div>
-        <div  className="md:ms-[50px]  h-[320px]">
+        <div  className="md:ms-[50px] group h-[320px]">
             <div className="w-[280px] slid relative h-[320px] mx-auto flex justify-center items-center border overflow-hidden border-[#32184b]">
-              <div className="w-[280px] slide absolute h-[320px] translate-x-[-270px] hover:translate-x-[-10px] transition-all ease-in flex justify-center items-center bg-[#cb23525c]">
+              <div className="w-[280px] slide absolute h-[320px] translate-x-[-270px] group-hover:translate-x-[-10px] transition-all ease-in flex justify-center items-center bg-[#cb23525c]">
                   <div className="flex flex-col flex-wrap ">
                         <p className="text-white ms-5 text-[20px] font-bold mt-2 ">Event 1</p>
+                        <p className='text-end text-white'>Read More<span></span></p>
                   </div>
               </div>  
               <div className="text-center">
@@ -18,6 +19,7 @@ const Card = ({img}) => {
               </h2>
               <br></br>
               <p className='text-white/65 text-[12px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, tempore.</p>
+              
               </div>
             </div>
           </div>
