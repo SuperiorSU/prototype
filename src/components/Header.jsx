7dd44be972d5e6d0dc5926805a5c71ba664a61e6
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-03-13 19:37:44 +0530

    hello this is new repo

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from  '../assets/ALogo.png'
+const Header = () => {
+  return (
+    <div>
+        <div className='flex flex-row justify-between p-5 items-center text-white bg-purple-950'>
+          <div>
+            <div className="logo">
+              <Image src={logo} height="50" alt=""/>
+            </div>
+          </div>
+          <div>
+            <ul className='flex gap-5 justify-center items-center list-none'>
+              <li>HOME</li>
+              <li>CAST</li>
+              <li>GALLERY</li>
+              <li>TEAMS</li>
+            </ul>
+          </div>
+          <div>
+            <Image src={logo} height="50" alt=""/>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
