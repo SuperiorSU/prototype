f3b81f513f692de0688abfa7736ea81361b0894d
SuperiorSU <sujalsinha2001@gmail.com>
2024-03-14 10:23:11 +0530

    new

--- src/components/TeamLeader.jsx
+++ src/components/TeamLeader.jsx
@@ -1,4 +1,6 @@
+"use client"
 import React from 'react'
+import Image from 'next/image';
 import ironman from "@/assets/3-2-iron-man-png.png";
 import wan from "@/assets/wandabg.jpg";
 import iron from "@/assets/ironmanbg.jpg";
@@ -15,13 +17,16 @@ import agent1 from "@/assets/agent1.png"
 import black from "@/assets/black.png"
 import natashabg from "@/assets/natashag.jpg"
 import blackbg from "@/assets/blackbg.jpg"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const TeamLeader = () => {
-    var settings = {
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToScroll: 1
       };
     const leader = [
         {
@@ -62,14 +67,31 @@ const TeamLeader = () => {
         },
         {
             id:6,
-            name: "Tony Stark",
-            alias: "IRON MAN",
-            image: ironman,
+            name: "Vision",
+            alias: "Vision",
+            image: vis,
         },
     ]
   return (
-    <div>
-
+    <div >
+        <Slider {...settings} >
+            {
+                leader.map((nam)=>{
+                    return (
+                        <div key={nam.id} className='group hover:border-red-500 border-[1px] p-4 rounded-md'>
+                            <div className='w-320px h-[480px]'>
+                                <Image src={nam.image}  className="w-full group-hover:scale-[1.1] transition-all duration-150 h-[330px] border-b-2 border-white"/>
+                                <p className='text-red-500 text-[24px] text-center p-3 font-bold'>{nam.alias} -({nam.name})</p>
+                                <div className='text-center'>
+                                     <input type='checkbox' id={`checkbox${nam.id}`} className='peer hidden'/><label htmlFor={`checkbox${nam.id}`} className='text-[20px] peer:bg-red-500 peer-checked:bg-red-500 peer-checked rounded-md text-white p-2 '>Select</label>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </Slider>
     </div>
   )
 }
